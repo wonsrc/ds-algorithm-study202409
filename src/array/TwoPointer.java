66f8862fb3fac74c5c6d755b531bd4c7a952f4e3
wonsrc <96459910+wonsrc@users.noreply.github.com>
2024-09-20 17:49:41 +0900

    ds-algorithm-study day-02 투포인터

---
+++ src/array/TwoPointer.java
@@ -0,0 +1,32 @@
+package array;
+
+import java.sql.SQLOutput;
+
+public class TwoPointer {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int targetSum = 5;
+        int start = 0, end = 0, sum = 0, count = 0;
+
+        while(end < arr.length) {
+            if(sum == targetSum) {
+                count++;
+                sum += arr[end++];
+            } else if (sum > targetSum) {   // 합이 목표값보다 크다면
+                sum -= arr[start++];
+            } else {    // 합이 목표값보다 작다면
+                sum += arr[end++];
+            }
+
+            System.out.println("start = " + start);
+            System.out.println("end = " + end);
+            System.out.println("count = " + count);
+            System.out.println("sum = " + sum);
+            System.out.println("=======================================");
+        }
+
+
+    }
+
+
+}
